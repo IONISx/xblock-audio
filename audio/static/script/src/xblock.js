78d698027131de44d6fb9f0b3c86bde11b147bdb
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-07-25 11:00:34 +0200

    Unwrap XBlock js endpoints

--- audio/static/script/src/xblock.js
+++ audio/static/script/src/xblock.js
@@ -1,17 +1,13 @@
 /* jshint unused: false */
 
 function AudioXBlockStudio(runtime, element) {
-    $(function () {
-        var server = new AudioXBlock.Server(runtime, element);
-        var view = new AudioXBlock.StudioView(server, runtime, element);
-        view.render();
-    });
+    var server = new AudioXBlock.Server(runtime, element);
+    var view = new AudioXBlock.StudioView(server, runtime, element);
+    view.render();
 }
 
 function AudioXBlockStudent(runtime, element) {
-    $(function () {
-        var server = new AudioXBlock.Server(runtime, element);
-        var view = new AudioXBlock.StudentView(server, runtime, element);
-        view.render();
-    });
+    var server = new AudioXBlock.Server(runtime, element);
+    var view = new AudioXBlock.StudentView(server, runtime, element);
+    view.render();
 }
