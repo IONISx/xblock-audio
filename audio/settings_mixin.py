253c2c5feb871e6866115a3c81f1d3504cbd7a4f
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-07-25 10:55:45 +0200

    Update editable_metadata_fields method

--- audio/settings_mixin.py
+++ audio/settings_mixin.py
@@ -44,50 +44,63 @@ class SettingsMixin(object):
     def editable_metadata_fields(self):
         """
         Returns the metadata fields to be edited in Studio. These are fields with scope `Scope.settings`.
-        """
-        def jsonify_value(field, json_choice):
-            if isinstance(json_choice, dict) and 'value' in json_choice:
-                json_choice = dict(json_choice)  # make a copy so below doesn't change the original
-                json_choice['value'] = field.to_json(json_choice['value'])
-            else:
-                json_choice = field.to_json(json_choice)
-                return json_choice
 
+        Can be limited by extending `non_editable_metadata_fields`.
+        """
         metadata_fields = {}
 
         # Only use the fields from this class, not mixins
         fields = getattr(self, 'unmixed_class', self.__class__).fields
 
         for field in fields.values():
+
             if field.scope != Scope.settings or field in self.non_editable_metadata_fields:
                 continue
 
-            # gets the 'default_value' and 'explicitly_set' attrs
-            metadata_fields[field.name] = self.runtime.get_field_provenance(self, field)
-            metadata_fields[field.name]['field_name'] = field.name
-            metadata_fields[field.name]['display_name'] = field.display_name
-            metadata_fields[field.name]['help'] = field.help
-            metadata_fields[field.name]['value'] = field.read_json(self)
-
-            # We support the following editors:
-            # 1. A select editor for fields with a list of possible values (includes Booleans).
-            # 2. Number editors for integers and floats.
-            # 3. A generic string editor for anything else (editing JSON representation of the value).
-            editor_type = "Generic"
-            values = field.values
-            if isinstance(values, (tuple, list)) and len(values) > 0:
-                editor_type = "Select"
-                values = [jsonify_value(field, json_choice) for json_choice in values]
-            elif isinstance(field, Integer):
-                editor_type = "Integer"
-            elif isinstance(field, Float):
-                editor_type = "Float"
-            elif isinstance(field, List):
-                editor_type = "List"
-            elif isinstance(field, Dict):
-                editor_type = "Dict"
-
-            metadata_fields[field.name]['type'] = editor_type
-            metadata_fields[field.name]['options'] = [] if values is None else values
+            metadata_fields[field.name] = self._create_metadata_editor_info(field)
 
         return metadata_fields
+
+    def _create_metadata_editor_info(self, field):
+        """
+        Creates the information needed by the metadata editor for a specific field.
+        """
+        def jsonify_value(field, json_choice):
+            if isinstance(json_choice, dict):
+                json_choice = dict(json_choice)  # make a copy so below doesn't change the original
+                if 'display_name' in json_choice:
+                    json_choice['display_name'] = json_choice['display_name']
+                if 'value' in json_choice:
+                    json_choice['value'] = field.to_json(json_choice['value'])
+            else:
+                json_choice = field.to_json(json_choice)
+            return json_choice
+
+        # gets the 'default_value' and 'explicitly_set' attrs
+        metadata_field_editor_info = self.runtime.get_field_provenance(self, field)
+        metadata_field_editor_info['field_name'] = field.name
+        metadata_field_editor_info['display_name'] = field.display_name
+        metadata_field_editor_info['help'] = field.help
+        metadata_field_editor_info['value'] = field.read_json(self)
+
+        # We support the following editors:
+        # 1. A select editor for fields with a list of possible values (includes Booleans).
+        # 2. Number editors for integers and floats.
+        # 3. A generic string editor for anything else (editing JSON representation of the value).
+        editor_type = "Generic"
+        values = field.values
+        if isinstance(values, (tuple, list)) and len(values) > 0:
+            editor_type = "Select"
+            values = [jsonify_value(field, json_choice) for json_choice in values]
+        elif isinstance(field, Integer):
+            editor_type = "Integer"
+        elif isinstance(field, Float):
+            editor_type = "Float"
+        elif isinstance(field, List):
+            editor_type = "List"
+        elif isinstance(field, Dict):
+            editor_type = "Dict"
+        metadata_field_editor_info['type'] = editor_type
+        metadata_field_editor_info['options'] = [] if values is None else values
+
+        return metadata_field_editor_info
